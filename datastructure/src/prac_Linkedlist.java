73f9b28274481bace09ba203132db9c5ad64f8af
Lohit Kumar Biswas <lohit.kumar.biswas@outlook.com>
2022-12-22 19:13:56 +0530

    New commit

--- datastructure/src/prac_Linkedlist.java
+++
@@ -1,5 +0,0 @@
-public class prac_Linkedlist {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
---
+++ recursion/src/fact.java
@@ -0,0 +1,11 @@
+public class rec1 {
+    static int fact(int n)
+    {
+        if(n>2)
+        {
+            return n*fact(n-1);
+
+        }
+        return n;
+    }
+}
