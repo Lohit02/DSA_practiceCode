77a12cd7b111f539c7dd5b05aeba87e34f6c7c33
Lohit Kumar Biswas <lohit.kumar.biswas@outlook.com>
2023-07-26 10:50:05 +0530

    new commit

---
+++ src/BinaryTree/Node.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class BinaryNode {
+    int data;
+    public BinaryNode right;
+    public BinaryNode left;
+    public int height;
+    public BinaryNode(int value)
+    {
+        this.data=value;
+    }
+}
